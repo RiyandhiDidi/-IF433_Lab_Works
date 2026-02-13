4bdea2cac718529e5949bbd462dd38d8bcd0c175
Riyandhi <riyandhi.adrian@gmail.com>
2026-02-13 15:34:15 +0700

    week01: (tugas 1) add legacy CircleCalculator code

--- src/oop_68383_RiyandhiAdrianDarmawan/week01/CircleCalculator.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week01/CircleCalculator.kt
@@ -1,2 +1,20 @@
 package oop_68383_RiyandhiAdrianDarmawan.week01
 
+fun main (args : Array<String>) {
+    val radius: Double = 7.0
+    var pi: Double = 3.14
+
+    var area: Double = pi * radius * radius
+
+    println("Radius: " + radius + ", Area: " + area)
+
+    checkSize(area)
+}
+
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a big circle")
+    } else {
+        println("This is a smaller circle")
+    }
+}
