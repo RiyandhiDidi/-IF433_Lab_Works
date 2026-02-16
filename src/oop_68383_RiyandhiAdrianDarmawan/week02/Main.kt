85630c0e28320f626af2ebc0dec10578cda06ca2
Riyandhi <riyandhi.adrian@gmail.com>
2026-02-16 13:24:48 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week02/Main.kt
@@ -0,0 +1,24 @@
+package oop_68383_riyandhiadriandarmawan.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Nama: ")
+    val name = scanner.nextLine()
+
+    print("NIM: ")
+    val nim = scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("NIM salah. Objek tidak dibuat.")
+        return
+    }
+
+    print("Jurusan: ")
+    val major = scanner.nextLine()
+
+    val student = student(name, nim, major)
+    println("Mahasiswa ${student.name} berhasil dibuat")
+}
