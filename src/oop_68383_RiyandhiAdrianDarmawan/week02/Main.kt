ddfa05107ba611f67c00bf6ba4d2fb485c361176
Riyandhi <riyandhi.adrian@gmail.com>
2026-02-16 12:16:34 +0700

    week02: add warning validation in init block

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week02/Main.kt
@@ -0,0 +1,11 @@
+package oop_68383_RiyandhiAdrianDarmawan.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(sourcer = System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    println("Masukkan nama")
+}
