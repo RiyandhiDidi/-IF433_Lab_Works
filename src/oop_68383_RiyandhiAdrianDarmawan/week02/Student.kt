65283a0e08d17c551728b8c9043149a80196e5bb
Riyandhi <riyandhi.adrian@gmail.com>
2026-02-16 13:23:43 +0700

    week02: add warning validation in init block

--- src/oop_68383_RiyandhiAdrianDarmawan/week02/Student.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week02/Student.kt
@@ -4,4 +4,10 @@ class Student(
     val name: String,
     val nim: String,
     var major: String
-)
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: NIM tidak valid")
+        }
+    }
+}
