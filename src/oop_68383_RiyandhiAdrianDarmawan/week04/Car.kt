07d771d0f904d63646e7bcc9aba285c7922ad870
Riyandhi <riyandhi.adrian@gmail.com>
2026-02-27 15:08:23 +0700

    week04: override honk method in Car

--- src/oop_68383_RiyandhiAdrianDarmawan/week04/Car.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week04/Car.kt
@@ -3,4 +3,9 @@ package oop_68383_RiyandhiAdrianDarmawan.week04
 class Car(
     brand: String,
     val numberOfDoors: Int
-) : Vehicle(brand)
+) : Vehicle(brand) {
+
+    override fun honk() {
+        println("$brand berbunyi: Toot toot!")
+    }
+}
