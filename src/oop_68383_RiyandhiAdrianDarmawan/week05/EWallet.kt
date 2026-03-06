328db098d21574cd0b80de77de3f1bdf83a39329
Riyandhi <riyandhi.adrian@gmail.com>
2026-03-06 13:52:44 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week05/EWallet.kt
@@ -0,0 +1,19 @@
+package oop_68383_RiyandhiAdrianDarmawan.week05
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil menggunakan EWallet. Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo sekarang: $balance")
+    }
+}
