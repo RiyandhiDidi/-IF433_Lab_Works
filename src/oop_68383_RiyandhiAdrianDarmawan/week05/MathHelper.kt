3f1455e5bb545adbc0bdb727d38995f1c7a3a660
Riyandhi <riyandhi.adrian@gmail.com>
2026-03-06 13:40:52 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week05/MathHelper.kt
@@ -0,0 +1,16 @@
+package oop_68383_RiyandhiAdrianDarmawan.week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
