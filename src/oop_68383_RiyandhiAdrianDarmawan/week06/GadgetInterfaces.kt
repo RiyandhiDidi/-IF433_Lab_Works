0f2546fde0e7a9127497ee7c916dedf1ad0566a0
Riyandhi <riyandhi.adrian@gmail.com>
2026-03-13 15:18:46 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week06/GadgetInterfaces.kt
@@ -0,0 +1,17 @@
+package oop_68383_RiyandhiAdrianDarmawan.week06
+
+interface Camera {
+
+    fun takePhoto() {
+        println("Taking photo...")
+    }
+
+}
+
+interface Phone {
+
+    fun takePhoto() {
+        println("Phone taking photo...")
+    }
+
+}
