da1178812c5505ee232bf9e80ebff7ac961554c6
Riyandhi <riyandhi.adrian@gmail.com>
2026-03-13 15:25:39 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week06/Main.kt
@@ -0,0 +1,15 @@
+package oop_68383_RiyandhiAdrianDarmawan.week06
+
+fun processCheckout(payment: PaymentMethod, amount: Double) {
+    payment.processPayment(amount)
+}
+
+fun main() {
+
+    val gopay = Gopay()
+    val creditCard = CreditCard()
+
+    processCheckout(gopay, 50000.0)
+    processCheckout(creditCard, 100000.0)
+
+}
