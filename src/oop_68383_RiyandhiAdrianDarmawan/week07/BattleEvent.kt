da4169cd76b70fe4060b4c78ab0531f284616ddd
Riyandhi <riyandhi.adrian@gmail.com>
2026-03-27 15:29:28 +0700

    week07: (task) create sealed class BattleState

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week07/BattleEvent.kt
@@ -0,0 +1,13 @@
+package oop_68383_RiyandhiAdrianDarmawan.week07
+
+sealed class BattleState {
+
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+
+    data class LootDropped(val item: GameItem) : BattleState()
+
+    data class GameOver(val reason: String) : BattleState()
+
+    object SafeZone : BattleState()
+
+}
