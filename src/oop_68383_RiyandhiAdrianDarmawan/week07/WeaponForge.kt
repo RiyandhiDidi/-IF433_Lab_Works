7b604239fa4fae25fe94010fc0297e7bd93ae39b
Riyandhi <riyandhi.adrian@gmail.com>
2026-03-27 15:28:04 +0700

    week07: (task) implement companion object factory in Weapon

--- src/oop_68383_RiyandhiAdrianDarmawan/week07/WeaponForge.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week07/WeaponForge.kt
@@ -3,4 +3,24 @@ package oop_68383_RiyandhiAdrianDarmawan.week07
 class Weapon private constructor(
     val item: GameItem,
     val durability: Int
-)
+) {
+
+    companion object {
+
+        fun forgeStarterSword(): Weapon {
+            return Weapon(
+                GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON),
+                50
+            )
+        }
+
+        fun forgeEpicSword(): Weapon {
+            return Weapon(
+                GameItem("Pedang Dewa", 100, ItemRarity.EPIC),
+                200
+            )
+        }
+
+    }
+
+}
