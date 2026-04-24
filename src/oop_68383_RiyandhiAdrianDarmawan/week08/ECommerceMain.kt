6cb92d561714c8f42e0643c50cf347a15732322a
Riyandhi <riyandhi.adrian@gmail.com>
2026-04-24 15:08:33 +0700

    week09: create immutable and mutable list

--- src/oop_68383_RiyandhiAdrianDarmawan/week08/ECommerceMain.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week08/ECommerceMain.kt
@@ -21,4 +21,4 @@ fun main() {
             println("Error: ${e.message}")
         }
     }
-} //
+}
---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week09/CollectionsBasics.kt
@@ -0,0 +1,11 @@
+package oop_68383_RiyandhiAdrianDarmawan.week09
+
+fun main() {
+
+    val numbers = listOf(1, 2, 3)
+    println(numbers)
+
+    val mutableNumbers = mutableListOf(1, 2, 3)
+    mutableNumbers.add(4)
+    println(mutableNumbers)
+}
