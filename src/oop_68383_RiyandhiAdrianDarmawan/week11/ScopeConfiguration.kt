c23279fe28433e32e5437f1eb673c8a7e35b46ab
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-08 11:23:45 +0700

    week11: implement apply for object initialization

--- src/oop_68383_RiyandhiAdrianDarmawan/week11/ScopeConfiguration.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week11/ScopeConfiguration.kt
@@ -3,4 +3,14 @@ package oop_68383_RiyandhiAdrianDarmawan.week11
 class User {
     var name: String = ""
     var age: Int = 0
+
+    fun main() {
+
+        val user = User().apply {
+            name = "Riyandhi"
+            age = 20
+        }
+
+        println(user.name)
+    }
 }
