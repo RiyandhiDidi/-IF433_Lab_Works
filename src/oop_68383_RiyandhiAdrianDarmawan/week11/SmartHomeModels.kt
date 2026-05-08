b051c8b00a5dda0ac7aa3c9eb4437fedee76eec6
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-08 11:27:29 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_68383_RiyandhiAdrianDarmawan.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
