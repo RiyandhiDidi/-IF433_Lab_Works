4b0291381435cf4054c04d9e032d3f4be6e1a915
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-08 10:59:38 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week10/WalletRepository.kt
@@ -0,0 +1,14 @@
+package oop_68383_RiyandhiAdrianDarmawan.week10
+
+class WalletRepository<T> {
+
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
