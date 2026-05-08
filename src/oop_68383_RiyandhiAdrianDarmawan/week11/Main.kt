206b53e31927196c8735be106c8fbfd9eefb3157
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-08 11:19:19 +0700

    week11: test extension functions in main

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_68383_RiyandhiAdrianDarmawan.week11
+
+fun main() {
+
+    val name = "Riyandhi"
+    println(name.greet())
+
+    val emptyText: String? = null
+    println(emptyText.safeLength())
+}
