d1aa672e1cc4bc6dc9bfd51e9df0322d37e87fda
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-08 10:50:29 +0700

    week10: instantiate Box with Int and String types

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week10/Main.kt
@@ -0,0 +1,10 @@
+package oop_68383_RiyandhiAdrianDarmawan.week10
+
+fun main() {
+
+    val intBox = Box(100)
+    println(intBox.getValue())
+
+    val stringBox = Box("Hello Generic")
+    println(stringBox.getValue())
+}
