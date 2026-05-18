e44669220cf6bc86da921efeb281b05eb67cd6b0
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-18 12:12:47 +0700

    week12: implement throw keyword for validation

--- src/oop_68383_RiyandhiAdrianDarmawan/week12/CustomExceptions.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week12/CustomExceptions.kt
@@ -1,3 +1,20 @@
 package oop_68383_RiyandhiAdrianDarmawan.week12
 
 class InsufficientFundsException(message: String) : Exception(message)
+
+class BankAccount(
+    private var balance: Int
+) {
+
+    fun withdraw(amount: Int) {
+
+        if (amount > balance) {
+            throw InsufficientFundsException(
+                "Saldo tidak cukup!"
+            )
+        }
+
+        balance -= amount
+        println("Withdraw berhasil")
+    }
+}
