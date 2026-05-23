6cadcaf4273af8644d970828c7fc2cd8e88f6f0d
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-23 19:03:04 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week13/CryptoTradeLogger.kt
@@ -0,0 +1,38 @@
+package oop_68383_RiyandhiAdrianDarmawan.week13
+
+import java.io.File
+
+fun main() {
+
+    val trades = listOf(
+
+        TradeRecord(
+            "TRX001",
+            "BTCUSDT",
+            "LONG",
+            150.0,
+            20
+        ),
+
+        TradeRecord(
+            "TRX002",
+            "ETHUSDT",
+            "SHORT",
+            -50.0,
+            10
+        ),
+
+        TradeRecord(
+            "TRX003",
+            "SOLUSDT",
+            "LONG",
+            80.0,
+            15
+        )
+    )
+
+    saveTrades(
+        trades,
+        "crypto_trades.csv"
+    )
+}
