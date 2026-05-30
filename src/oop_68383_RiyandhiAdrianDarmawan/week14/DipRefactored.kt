2d5b161105267dc791430218e68043a69777b476
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-30 13:42:54 +0700

    week14: create Database abstraction and implementations

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week14/DipRefactored.kt
@@ -0,0 +1,20 @@
+package oop_68383_RiyandhiAdrianDarmawan.week14
+
+interface Database {
+
+    fun save()
+}
+
+class MySqlDb : Database {
+
+    override fun save() {
+        println("Save MySQL")
+    }
+}
+
+class MongoDb : Database {
+
+    override fun save() {
+        println("Save MongoDB")
+    }
+}
