2bef576b706eba90bc054200d6feaaa37090b7d0
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-30 13:40:41 +0700

    week14: segregate device interfaces

---
+++ src/oop_68383_RiyandhiAdrianDarmawan/week14/IspRefactored.kt
@@ -0,0 +1,16 @@
+package oop_68383_RiyandhiAdrianDarmawan.week14
+
+interface Printable {
+
+    fun print()
+}
+
+interface Scannable {
+
+    fun scan()
+}
+
+interface Faxable {
+
+    fun fax()
+}
