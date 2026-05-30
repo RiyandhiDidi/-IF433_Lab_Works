de78557f26e637e96fe32abee9d91eb3637a6745
Riyandhi <riyandhi.adrian@gmail.com>
2026-05-30 13:44:54 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_68383_RiyandhiAdrianDarmawan/week14/EcommerceTask.kt
+++ src/oop_68383_RiyandhiAdrianDarmawan/week14/EcommerceTask.kt
@@ -21,3 +21,32 @@ class BadOrderProcessor {
         println("Email sent")
     }
 }
+
+interface OrderRepository {
+
+    fun saveOrder(price: Double)
+}
+
+class CsvOrderRepository : OrderRepository {
+
+    override fun saveOrder(price: Double) {
+        println("Order saved: $price")
+    }
+}
+
+interface NotificationService {
+
+    fun sendNotification()
+}
+
+class EmailNotifier : NotificationService {
+
+    override fun sendNotification() {
+        println("Email sent")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+)
